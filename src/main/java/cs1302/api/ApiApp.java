8236e76fa7e96b1082028d08210620100f295959
Jason Lee <jyl03321@uga.edu>
2023-04-30 12:43:56 -0400

    Second commit

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -9,9 +9,28 @@ import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.text.Text;
 import java.lang.Runnable;
 import java.lang.Thread;
+
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.net.http.HttpResponse.BodyHandlers;
+
+
+import java.io.IOException;
+import java.lang.InterruptedException;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -24,7 +43,21 @@ public class ApiApp extends Application {
     Scene scene;
     VBox root;
 
-    T
+    Textfield textField;
+    Button button;
+
+
+    /** HTTP client. */
+    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)           // uses HTTP protocol version 2 where possible
+        .followRedirects(HttpClient.Redirect.NORMAL)  // always redirects, except from HTTPS to HTTP
+        .build();                                     // builds and returns a HttpClient object
+
+    /** Google {@code Gson} object for parsing JSON-formatted strings. */
+    public static Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()                          // enable nice output when printing
+        .create();                                    // builds and returns a Gson object
+
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
@@ -32,7 +65,17 @@ public class ApiApp extends Application {
      */
     public ApiApp() {
         root = new VBox();
+
+        this.textField = new TextField();
+        this.button = new Button("Go");
     } // ApiApp
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void init() {
+        System.out.println("init() is called");
+    } // init
 
     /** {@inheritDoc} */
     @Override
@@ -61,5 +104,22 @@ public class ApiApp extends Application {
         stage.show();
 
     } // start
+
+    /**
+     * This method is used to make a new thread.
+     * @param task is the method that is run on a separate thread.
+     */
+    public static void runInNewThread(Runnable task) {
+        Thread t = new Thread(task);
+        t.setDaemon(true);
+        t.start();
+    } // runInNewThread
+
+    /** {@inheritDoc} */
+    @Override
+    public void stop() {
+        // feel free to modify this method
+        System.out.println("stop() called");
+    } // stop
 
 } // ApiApp
